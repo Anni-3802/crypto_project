673c8e8f563dcd96d2a9a04576b38be8f32a84dc
ayush kumar <kumarayush40633@gmail.com>
2025-04-26 13:38:29 +0530

    card componnent

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { api } from '../api/auth';
+import { HomepageCard } from '../components/CardsComponents';
 import { AuthContext } from '../utils/AuthContext';
 
 const Home = () => {
@@ -8,23 +8,31 @@ const Home = () => {
   const { isLogined } = useContext(AuthContext)
 
   useEffect(() => {
-    api.get("/crypto/top").then((res) => { setCoins(res.data) });
+    api.get("/crypto/top").then((res) => { setCoins(res.data.slice(0, 4)) });
   }, []);
 
-
   return (
-    <div>
-      <h2>Top Coins</h2>
-      <ul>
-        {coins.map(coin => (
-          <li key={coin.id}>
-            {isLogined ? <Link to={`/coin/${coin.id}`}>{coin.name}</Link> :
-              <span>{coin.name} (Login to view details)</span>}
-          </li>
-         
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className='container-fluid'>
+        <section id='banner-section'>
+          <div>
+            <h2 className='headingText'>Welcome To start A New jounery With WEb3 </h2>
+          </div>
+
+        </section>
+        <section id="top-coins">
+          <h2 className='mb-2 headingText'>Top Coins</h2>
+          <div className='row'>
+            {coins.length > 0 && coins.map(coin => (
+              <div className='col-3 mb-2' key={coin.id} >
+                {isLogined ? <HomepageCard cardtitle={coin.name} cardlink={`/coin/${coin.id}`} islogined={true} /> : <HomepageCard cardtitle={coin.name} />}
+              </div>
+            ))}
+          </div>
+        </section>
+
+      </div>
+    </>
   );
 };
 
